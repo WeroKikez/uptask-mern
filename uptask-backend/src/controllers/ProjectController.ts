703a57d7a2d5ae67cc265e5e9b28b20d2d2ff277
Emiliano Enriquez <enriquez.emiliano15@gmail.com>
2024-07-11 23:04:41 -0600

    Backend: Permitir que colaboradores vean proyectos en los que forman parte

--- uptask-backend/src/controllers/ProjectController.ts
+++ uptask-backend/src/controllers/ProjectController.ts
@@ -20,7 +20,8 @@ export class ProjectController {
         try {
             const projects = await Project.find({
                 $or: [
-                    { manager: { $in: req.user.id} }
+                    { manager: { $in: req.user.id} },
+                    { team: { $in: req.user.id} }
                 ]
             })
 
@@ -40,7 +41,7 @@ export class ProjectController {
                 return res.status(404).json({ error: error.message })
             }
 
-            if(project.manager.toString() !== req.user.id.toString()) {
+            if(project.manager.toString() !== req.user.id.toString() && !project.team.includes(req.user.id)) {
                 const error = new Error('Acción No Válida')
                 return res.status(401).json({ error: error.message })
             }
