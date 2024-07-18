f194934b371545dfb99d7f9b43e6731b34a8eec0
Emiliano Enriquez <enriquez.emiliano15@gmail.com>
2024-07-18 13:25:49 -0600

    Cambios para subir server a render

--- uptask-backend/src/config/cors.ts
+++ uptask-backend/src/config/cors.ts
@@ -3,6 +3,7 @@ import { CorsOptions } from 'cors'
 export const corsConfig: CorsOptions = {
     origin: function(origin, callback) {
         const whiteList = [process.env.FRONTEND_URL]
+        console.log(origin)
 
         if(process.argv[2] === '--api') { 
             whiteList.push(undefined)
--- uptask-backend/src/utils/jwt.ts
+++ uptask-backend/src/utils/jwt.ts
@@ -1,10 +1,10 @@
 import jwt from 'jsonwebtoken'
-import Types from 'mongoose'
-
+import mongoose from 'mongoose'
+ 
 type UserPayload = {
-    id: Types.ObjectId
+    id: mongoose.Schema.Types.ObjectId
 }
-
+  
 export const generateJWT = (payload : UserPayload) => {
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
         expiresIn: '180d'
